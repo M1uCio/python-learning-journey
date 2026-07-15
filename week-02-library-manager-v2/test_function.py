55a59ab58b326988a729029ef709d95221174c5a
M1uCio <maciejzuba123@gmail.com>
2026-07-15 22:07:02 +0200

    I added test_str functions, which checks whether inputing str for square function returns TypeError

--- week-02-library-manager-v2/test_function.py
+++ week-02-library-manager-v2/test_function.py
@@ -1,3 +1,5 @@
+import pytest
+
 from unit_tests import square
 
 def test_square():
@@ -7,3 +9,7 @@ def test_square():
     assert square(-2) == 4
     assert square(-3) == 9
     assert square(0) == 0
+
+def test_str():
+    with pytest.raises(TypeError):
+        square("cat")
